668abb610480763b912d5d5866531500f5ca957f
Janson Kang <temasek71@gmail.com>
2015-04-19 20:14:23 +0800

    hlte-common: Enable transparent compression

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -104,3 +104,6 @@ WIFI_DRIVER_MODULE_AP_ARG   := "firmware_path=/system/etc/wifi/bcmdhd_apsta.bin 
 WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/dhd/parameters/firmware_path"
 WIFI_DRIVER_FW_PATH_STA     := "/system/etc/wifi/bcmdhd_sta.bin"
 WIFI_DRIVER_FW_PATH_AP      := "/system/etc/wifi/bcmdhd_apsta.bin"
+
+# Enable transparent compression in the build
+TARGET_TRANSPARENT_COMPRESSION_METHOD := lz4
